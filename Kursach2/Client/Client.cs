9532f76857314b41d3e55dcc03dd51ee5436ace6
Egor251 <egorpyshny@gmail.com>
2023-05-05 10:43:57 +0300

    fix

--- Kursach2/Client/Client.cs
+++ Kursach2/Client/Client.cs
@@ -29,7 +29,7 @@ namespace Kursach2.Client
             {
                 try
                 {
-                    client.Connect(IPAddress.Parse("192.168.0.104"), 8001);
+                    client.Connect(IPAddress.Parse("192.168.43.137"), 8001);
                     ConnectionToServer = true;
                     client.GetStream().Write(new byte[] { 0 }, 0, 1);
                     byte[] temp = new byte[1];
